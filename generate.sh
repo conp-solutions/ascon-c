4b316a849edea7c9ae3109a82c0460707e76b57c
Norbert Manthey <nmanthey@conp-solutions.com>
2023-04-05 22:54:23 +0200

    generate: also generate unsat
    Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

--- generate.sh
+++ generate.sh
@@ -5,15 +5,18 @@ set -e
 
 mkdir -p cnfs
 
-for bytes in {4..13}
+for U in "" "-u"
 do
-  for missing in {1..2}
+  for bytes in {4..13}
   do
-    ./generate_formulas.py -n $bytes -m $missing
-    rm asconhashv12_opt64_*.c
-    f=$(ls asconhashv12_opt64*.cnf)
-    f=$(basename $f)
-    grep -v "^c" $f > cnfs/$f
-    rm $f
+    for missing in {1..2}
+    do
+      ./generate_formulas.py -n $bytes -m $missing $U
+      rm asconhashv12_opt64_*.c
+      f=$(ls asconhashv12_opt64*.cnf)
+      f=$(basename $f)
+      grep -v "^c" $f > cnfs/$f
+      rm $f
+    done
   done
 done
